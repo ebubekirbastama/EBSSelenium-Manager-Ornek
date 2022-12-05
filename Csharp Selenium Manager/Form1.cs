0e1633a3dafb423d963efce987cbc5035daf1491
Ebubekir Bastama <ebubekirbastama@elmalicesmekuruyemis.com>
2022-12-05 07:42:01 +0300

    Add files via upload

---
+++ Csharp Selenium Manager/Form1.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Windows.Forms;
+
+namespace Csharp_Selenium_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SeleniumManager.DriverPath("chromedriver");
+
+            ChromeDriver drv = new ChromeDriver();
+            drv.Navigate().GoToUrl("https://www.ebubekirbastama.com");
+        }
+    }
+}
